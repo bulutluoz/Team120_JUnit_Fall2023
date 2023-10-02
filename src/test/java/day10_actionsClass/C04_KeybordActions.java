d98a648fb9bfef8c1046b9b651a2346df7816190
bulutluoz <abbulutluoz@gmail.com>
2023-10-02 20:40:36 +0200

    10.gun sonu

---
+++ src/test/java/day10_actionsClass/C04_KeybordActions.java
@@ -0,0 +1,44 @@
+package day10_actionsClass;
+
+import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.interactions.Actions;
+import utilities.TestBase;
+
+public class C04_KeybordActions extends TestBase {
+
+    @Test
+    public void test01(){
+        //2- https://www.amazon.com sayfasina gidelim
+        driver.get("https://www.amazon.com");
+        //3- Arama kutusuna actions method’larini kullanarak
+        //   Samsung A71 yazdirin ve Enter’a basarak arama yaptirin
+
+        WebElement aramaKutusu = driver.findElement(By.id("twotabsearchtextbox"));
+
+        Actions actions = new Actions(driver);
+
+        actions
+                .click(aramaKutusu)
+                .keyDown(Keys.SHIFT)
+                .sendKeys("s")
+                .keyUp(Keys.SHIFT)
+                .sendKeys("amsung ")
+                .keyDown(Keys.SHIFT)
+                .sendKeys("a")
+                .keyUp(Keys.SHIFT)
+                .sendKeys("71")
+                .sendKeys(Keys.ENTER)
+                .perform();
+
+        //4- aramanin gerceklestigini test edin
+
+        String expectedIcerik = "Samsung A71";
+        String actualSonucYazisi = driver.findElement(By.xpath("//h1[@class='a-size-base s-desktop-toolbar a-text-normal']"))
+                                    .getText();
+
+        bekle(3);
+    }
+}
